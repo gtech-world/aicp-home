85a09a6f26a98e93cb0353cea974aef4c28373f5
eericxu <2681350846@qq.com>
2023-09-18 17:33:15 +0800

    fix: moment error

--- config/config.ts
+++ config/config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
    */
   history: { type: 'browser' },
   exportStatic: {},
+  ignoreMomentLocale: false,
   /**
    * @name 兼容性设置
    * @description 设置 ie11 不一定完美兼容，需要检查自己使用的所有依赖
