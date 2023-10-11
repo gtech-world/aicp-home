3c34a05faf1005142f206faa25dbb2066f3aed97
YongZL <1679963578@qq.com>
2023-10-11 10:34:57 +0800

    fix: ui

--- src/components/routes/user.tsx
+++ src/components/routes/user.tsx
@@ -17,28 +17,32 @@ export const User = () => {
   ];
 
   return (
-    <>
-      <div className="flex flex-wrap bg-[#FFFFFF]">
-        <Fragment>
-          {label.map((e, i) => {
-            return (
-              <div className="w-1/3 p-4" key={`user_${i}`}>
-                {e.label}
-                <div className="mt-2">
-                  <Input className=" w-[360px]" value={e.name} disabled />
+    <Fragment>
+      <div className="  bg-[#FFFFFF] ">
+        <div className="text-[#000000D9] leading-6 text-[16px] font-semibold  items-center p-5">用户管理</div>
+        <div className=" border-b-[1.5px]"></div>
+        <div className="flex flex-wrap ">
+          <Fragment>
+            {label.map((e, i) => {
+              return (
+                <div className="w-1/3 p-4" key={`user_${i}`}>
+                  {e.label}
+                  <div className="mt-2">
+                    <Input className=" w-[360px]" value={e.name} disabled />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </Fragment>
-        <div className="flex flex-row items-center mb-2 ml-5">
-          <Detail fill="#29953A" className="w-[6.125rem] mb-5" />
-          <span className="text-[#BFBFBF] ml-2 mo:text-[11px]">
-            请确认用户信息真实、准确。如有组织机构信息更新，请及时联系AICP平台运营管理人员。
-          </span>
+              );
+            })}
+          </Fragment>
+          <div className="flex flex-row items-center mb-2 ml-5">
+            <Detail fill="#29953A" className="w-[6.125rem] mb-5" />
+            <span className="text-[#BFBFBF] ml-2 mo:text-[11px]">
+              请确认用户信息真实、准确。如有组织机构信息更新，请及时联系AICP平台运营管理人员。
+            </span>
+          </div>
         </div>
       </div>
-    </>
+    </Fragment>
   );
 };
 export default User;
