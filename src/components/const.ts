fb24f2f38f1a999d48ee845518c402461bd5af00
colton <1679963578@qq.com>
2024-06-25 17:22:47 +0800

    feat: add switch languages

--- src/components/const.ts
+++ src/components/const.ts
@@ -8,3 +8,18 @@ export const SIZE = {
   small: 'py-1',
   big: 'py-3',
 };
+
+export const LngList = [
+  {
+    lang: 'en-US',
+    label: 'English',
+    icon: '🇺🇸',
+    title: 'Language',
+  },
+  {
+    lang: 'zh-CN',
+    label: '简体中文',
+    icon: '🇨🇳',
+    title: '语言',
+  },
+];
--- src/components/routes/label.tsx
+++ src/components/routes/label.tsx
@@ -13,6 +13,8 @@ export function Label() {
   const [sq] = useSearchParams();
   const vin: string = sq.get('vin') as string;
   const { data: tagInfo, isLoading } = useTagInfo(vin);
+  const { t } = useT();
+
   const {
     evaluationAgency = '-',
     evaluationExpireTime = '-',
@@ -26,57 +28,56 @@ export function Label() {
     evaluationBoundary = '-',
     evaluationBasis = '-',
     pcfResult = '-',
-    certificateNumber = '当前无证书',
+    certificateNumber = t('0000001'),
   } = tagInfo || {};
+
   const productInfo = [
-    { text: '标签编号', value: uuid },
-    { text: '产品型号', value: productName },
-    { text: '产品唯一标识符', value: loadNumber },
-    { text: '标签申请主体', value: orgName },
+    { text: t('Label Number'), value: uuid },
+    { text: t('Product System'), value: productName },
+    { text: t('Product UID'), value: loadNumber },
+    { text: t('Label Applicant'), value: orgName },
   ];
 
   const productTagInfo = [
-    { text: '评价类型', value: evaluationType !== 'aicp' ? '第三方认证' : 'AICP平台审核' },
-    { text: '证书编号', value: certificateNumber },
-    { text: '功能单位', value: functionalUnit },
-    { text: '评价边界', value: evaluationBoundary },
-    { text: '评价依据', value: evaluationBasis },
-    { text: '评价机构', value: evaluationAgency },
-    { text: '评价有效期', value: evaluationExpireTime },
+    { text: t('Evaluation'), value: evaluationType !== 'aicp' ? t('Third-party Certification') : 'AICP平台审核' },
+    { text: t('Certification Number'), value: certificateNumber },
+    { text: t('Functional Unit'), value: functionalUnit },
+    { text: t('Assessment Boundary'), value: evaluationBoundary },
+    { text: t('Reference Standard'), value: evaluationBasis },
+    { text: t('Evaluation by'), value: evaluationAgency },
+    { text: t('Effective Date'), value: evaluationExpireTime },
     {
-      text: '区块链SBT证书编号',
+      text: t('Blockchain SBT Number'),
       value: tokenId,
       link: genScanUrl('address', '0x7BC6afe0cDc6DE9191dfC6d68A3bad45E270F695'),
     },
   ];
 
-  const { t } = useT();
-
   const noHeader = () => {
     return (
       <Fragment>
         <div className="flex w-full gap-5 mo:flex-none mo:flex-col ">
-          <div className="w-[420px] max-h-[450px] p-5 mo:h-auto  bg-[#FFFFFF] text-center  flex justify-center mo:justify-start   mo:flex-row mo:w-full flex-col items-center rounded-lg ">
-            <SVGCarbon3 className="text-[5.375rem] w-[5.375rem] mt-[.625rem] mb-5 mo:mb-2  shrink-0" />
+          <div className="w-[420px] max-h-[480px] p-5 mo:h-auto  bg-[#FFFFFF] text-center  flex justify-center mo:justify-start   mo:flex-row mo:w-full flex-col items-center rounded-lg ">
+            <img src="label.jpg" className="text-[5.375rem] w-[5.375rem] mt-[.625rem] mb-5 mo:mb-2  shrink-0" />
             <div className="mo:ml-[2.5rem] mo:flex-1 ">
               <div className="text-[#29953A] text-[1.75rem] leading-8 font-semibold max-w-full break-all">
                 {pcfResult}
               </div>
-              <div className="font-[1.75rem] leading-8   ">二氧化碳等效排放</div>
+              <div className="font-[1.75rem] leading-8   ">{t('Cradle-to-Grave Footprint')}</div>
             </div>
           </div>
-          <div className="w-[420px]  mo:w-full  max-h-[450px] bg-[#FFFFFF] mo:max-h-[350px] rounded-lg">
+          <div className="w-[420px]  mo:w-full  max-h-[480px] bg-[#FFFFFF] mo:max-h-[350px] rounded-lg">
             <div className="mx-5 mt-5 mb-5 mmd:mt-10">
               <div className="mb-5 mo:mb-[15px]  md:text-[18px]   font-bold mo:text-[18px] text-[20px] leading-7">
-                产品信息
+                {t('Product Info')}
               </div>
               <DivText textArray={productInfo} />
             </div>
           </div>
-          <div className="w-[420px]  mo:w-full  max-h-[450px] bg-[#FFFFFF] mo:max-h-[370px] rounded-lg">
+          <div className="w-[420px]  mo:w-full  max-h-[480px] bg-[#FFFFFF] mo:max-h-[370px] rounded-lg">
             <div className="mx-5 mt-5 mb-5 mmd:mt-10">
               <div className="mb-5 mo:mb-[15px] md:text-[18px]   font-bold mo:text-[18px] text-[20px] leading-7">
-                产品碳足迹评价信息
+                {t('Product Carbon Footprint')}
               </div>
               <DivText textArray={productTagInfo} />
             </div>
@@ -84,7 +85,8 @@ export function Label() {
         </div>
         <div className="flex gap-5 mt-5 mo:flex-none mo:flex-col ">
           <div className="w-[640px] py-5 mo:pt-[1.875rem] mo:w-full bg-[#FFFFFF] flex flex-col items-center rounded-lg gap-5 mo:gap-[.9375rem]">
-            <SVGCarbon3 className="text-[5.375rem] w-[5.375rem]" />
+            {/* <SVGCarbon3 className="text-[5.375rem] w-[5.375rem]" /> */}
+            <img src="label.jpg" className="text-[5.375rem] w-[5.375rem]" />
             <div
               className="text-lg font-bold mo:mx-5 "
               dangerouslySetInnerHTML={{
@@ -96,7 +98,7 @@ export function Label() {
               dangerouslySetInnerHTML={{
                 __html: handleCarbonStr(
                   t(
-                    'The AIAG Digital3 Carbon Trust Label is an industry-level certification framework for every vehicle produced under {{value}}. The Trust Label guarantees that any raw data behind the label is verified and recorded in an immutable manner for the ultimate transparency and traceability for the vehicle’s carbon performance.',
+                    'An AIAG EPD/CFP Label stands for an EPD/CFP declaration that is registered on the AIAG EPD Platform and recognized by AIAG and IAOB.',
                   ).replace(
                     '{{value}}',
                     `<a class="text-green-2 cursor-pointer" target="_blank" href="https://aiag.org.cn/ACAC/Automotive-Carbon-Advisory-Committee" rel="noreferrer">${t(
@@ -115,7 +117,7 @@ export function Label() {
               className="text-[.9375rem] font-normal text-center mx-5"
               dangerouslySetInnerHTML={{
                 __html: t(
-                  'A Soul-bounded Token (a special type of NFT that is not allowed to transfer after created) has been generated on blockchain to make sure the information in this label is immutable and will be maintain for traceability forever. Check {{value}} to verify the SBT on blockchain explorer.',
+                  'Every AIAG EPD/CFP Label is backed by an dedicated NFT on blockchain to guarantee long term immutability and traceability on the underlying data. Check {{value}} to view NFT information from the blockchain explorer.',
                 ).replace(
                   '{{value}}',
                   `<a class="text-green-2 cursor-pointer" href="${wrapPath(
@@ -145,7 +147,7 @@ export function Label() {
           ) : (
             <Fragment>
               <div className="mb-5 text-2xl font-bold leading-normal">
-                {t('Product Carbon Footprint Certified')} <span className="text-base font-medium">{t('by AIAG')}</span>
+                {t('CFP Label')} <span className="text-base font-medium">{t('Recognized by AIAG & IAOB')}</span>
               </div>
               {noHeader()}
             </Fragment>
